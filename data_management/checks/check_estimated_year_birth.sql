856f66cfe6c5dcad6a1b169931899a5d4820a89a
jedgroev <degroevejohannes@gmail.com>
2016-09-19 14:52:50 +0200

    Update estimated year of birth
    added query that checks consistency of first capture date between animals and captures table.

--- data_management/checks/check_estimated_year_birth.sql
+++ data_management/checks/check_estimated_year_birth.sql
@@ -1,6 +1,18 @@
 -- set of queries needed to detect inconsistencies in estimated year of birth, 
 -- that in turn is needed to associate the correct age class at each capture.
 
+-- Animals with inconsistent first capture date between animals & capture table 
+WITH x as (
+SELECT DISTINCT study_areas_id ,animals_id, animals_original_id, age_class_code_capture, first_capture_date first_capture_at, 
+min(capture_timestamp::date) over (partition by animals_id ORDER BY capture_timestamp) first_capture_ct 
+FROM main.animals JOIN main.animals_captures using (animals_id) ORDER BY study_areas_id, animals_id 
+)
+SELECT study_areas_id, animals_id, animals_original_id, age_class_code_capture, first_capture_at,
+first_capture_ct, first_capture_at - first_capture_ct diff 
+FROM x 
+WHERE first_capture_at != first_capture_ct 
+ORDER BY study_areas_id, animals_id  
+
 -- Animals without age class at capture
 SELECT study_areas_id, animals_id, animals_original_id, first_capture_date, age_class_code_capture,  year_birth, year_birth_exact
 FROM main.animals
