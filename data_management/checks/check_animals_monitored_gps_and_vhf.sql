e5ee240d67a174c0b56d69316f0bf4c94814a499
jedgroev <degroevejohannes@gmail.com>
2016-11-16 18:22:56 +0100

    Update check_animals_monitored_gps_and_vhf.sql

--- data_management/checks/check_animals_monitored_gps_and_vhf.sql
+++ data_management/checks/check_animals_monitored_gps_and_vhf.sql
@@ -13,3 +13,17 @@ FROM
   main.animals a JOIN main.gps_sensors_animals b USING (animals_id) JOIN main.vhf_sensors_animals c USING (animals_id)
 ORDER BY
   a.study_areas_id, a.animals_id;
+
+-- Is there a first_capture_date that is later then a start_time of gps or vhf for animals monitored with both? 
+WITH x AS (
+SELECT
+  a.animals_id, a.study_areas_id, a.first_capture_date, 
+  (first_capture_date - b.start_time) <= interval '00:00:00' deployment_after_first_capture_gps,
+  b.start_time, b.end_time, b.mortality_code, b.end_deployment_code, 
+  (first_capture_date - c.start_time) <= interval '00:00:00' deployment_after_first_capture_vhf,
+  c.start_time, c.end_time, c.end_deployment_code, c.mortality_code
+FROM 
+  main.animals a JOIN main.gps_sensors_animals b USING (animals_id) JOIN main.vhf_sensors_animals c USING (animals_id)
+ORDER BY
+  a.study_areas_id, a.animals_id)
+SELECT * FROM x WHERE (deployment_after_first_capture_gps = FALSE OR deployment_after_first_capture_vhf = FALSE)  
