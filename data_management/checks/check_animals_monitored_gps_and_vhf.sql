bf7a32071a56f627e86ea539da11e81c0e06ad3e
jedgroev <degroevejohannes@gmail.com>
2016-11-16 18:10:44 +0100

    Update check_animals_monitored_gps_and_vhf.sql

--- data_management/checks/check_animals_monitored_gps_and_vhf.sql
+++ data_management/checks/check_animals_monitored_gps_and_vhf.sql
@@ -6,24 +6,10 @@ SELECT *
 
 -- Select deployment info of animals monitored with both vhf and gps to verify that monitoring interval and capture dates are consistent
 SELECT
-  animals.animals_id, 
-  animals.study_areas_id, 
-  animals.first_capture_date, 
-  gps_sensors_animals.start_time, 
-  gps_sensors_animals.end_time, 
-  gps_sensors_animals.mortality_code, 
-  gps_sensors_animals.end_deployment_code, 
-  vhf_sensors_animals.start_time, 
-  vhf_sensors_animals.end_time, 
-  vhf_sensors_animals.end_deployment_code, 
-  vhf_sensors_animals.mortality_code
+  a.animals_id, a.study_areas_id, a.first_capture_date, 
+  b.start_time, b.end_time, b.mortality_code, b.end_deployment_code, 
+  c.start_time, c.end_time, c.end_deployment_code, c.mortality_code
 FROM 
-  main.animals, 
-  main.gps_sensors_animals, 
-  main.vhf_sensors_animals
-WHERE 
-  animals.animals_id = gps_sensors_animals.animals_id AND
-  animals.animals_id = vhf_sensors_animals.animals_id
+  main.animals a JOIN main.gps_sensors_animals b USING (animals_id) JOIN main.vhf_sensors_animals c USING (animals_id)
 ORDER BY
-  animals.study_areas_id, 
-  animals.animals_id
+  a.study_areas_id, a.animals_id;
