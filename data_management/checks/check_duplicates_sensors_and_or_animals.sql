7351e38b08cad33c8a98955c402944e89d304e33
jedgroev <degroevejohannes@gmail.com>
2016-11-16 17:18:46 +0100

    added queries for vhf duplicates and capture data

--- data_management/checks/check_duplicates_sensors_and_or_animals.sql
+++ data_management/checks/check_duplicates_sensors_and_or_animals.sql
@@ -1,5 +1,5 @@
 -- 1 DUPLICATE SENSORS 
-
+-- GPS 
 -- 1a List of all sensors with the same original id (in the same research group) -> potential duplicates
 SELECT a.*
 from
@@ -23,6 +23,32 @@ main.gps_sensors as a,
 (SELECT gps_sensors_original_id, research_groups_id FROM main.gps_sensors group by gps_sensors_original_id, research_groups_id having count(*) > 1) as b
 where a.gps_sensors_original_id = b.gps_sensors_original_id and a.research_groups_id = b.research_groups_id) order by gps_sensors_id;
 
+--VHF 
+-- 1a List of all sensors with the same original id (in the same research group) -> potential duplicates
+SELECT a.*
+from
+main.vhf_sensors as a,
+(SELECT vhf_sensors_original_id, research_groups_id FROM main.vhf_sensors group by vhf_sensors_original_id, research_groups_id having count(*) > 1) as b
+where a.vhf_sensors_original_id = b.vhf_sensors_original_id and a.research_groups_id = b.research_groups_id ORDER BY vhf_sensors_original_id;
+
+-- 1b Check which of those vhf_sensor_ids are used in the vhf_sensors_animals table 
+SELECT DISTINCT a.* from main.vhf_sensors_animals a where vhf_sensors_id in (
+SELECT vhf_sensors_id 
+from
+main.vhf_sensors as a,
+(SELECT vhf_sensors_original_id, research_groups_id FROM main.vhf_sensors group by vhf_sensors_original_id, research_groups_id having count(*) > 1) as b
+where a.vhf_sensors_original_id = b.vhf_sensors_original_id and a.research_groups_id = b.research_groups_id) order by vhf_sensors_id;
+
+
+-- 1c Check which of those vhf_sensor_ids are used in the vhf_data_animals table 
+SELECT DISTINCT vhf_sensors_id from main.vhf_data_animals where vhf_sensors_id in (
+SELECT vhf_sensors_id 
+from
+main.vhf_sensors as a,
+(SELECT vhf_sensors_original_id, research_groups_id FROM main.vhf_sensors group by vhf_sensors_original_id, research_groups_id having count(*) > 1) as b
+where a.vhf_sensors_original_id = b.vhf_sensors_original_id and a.research_groups_id = b.research_groups_id) order by vhf_sensors_id;
+
+ 
 -- 2 DUPLICATE ANIMALS 
 -- 2a List of all animals with the same original id (in the same study area) -> potential duplicates
 SELECT a.* 
@@ -50,8 +76,45 @@ where a.animals_original_id = b.animals_original_id and a.study_areas_id = b.stu
 
 -- 3 DUPLICATE ANIMALS-SENSORS 
 -- 3a List of all animals-sensors with duplicate animals-sensors id (possible) -> check start and end times to confirm potential duplicates
+-- GPS
 SELECT a.*
 from
 main.gps_sensors_animals as a,
 (SELECT animals_id, gps_sensors_id FROM main.gps_sensors_animals group by gps_sensors_id, animals_id having count(*) > 1) as b
 where a.animals_id = b.animals_id and a.gps_sensors_id = b.gps_sensors_id;
+-- VHF
+SELECT a.*
+from
+main.vhf_sensors_animals as a,
+(SELECT animals_id, vhf_sensors_id FROM main.vhf_sensors_animals group by vhf_sensors_id, animals_id having count(*) > 1) as b
+where a.animals_id = b.animals_id and a.vhf_sensors_id = b.vhf_sensors_id;
+
+
+
+-- 4 DUPLICATED CAPTURES 
+-- Are there duplicate capture events? 
+SELECT count(*), study_areas_id, animals_id, capture_timestamp
+FROM main.animals_captures JOIN main.animals using (animals_id)
+GROUP BY animals_id, capture_timestamp, study_areas_id
+HAVING count(*) > 1
+ORDER BY study_areas_id, animals_id
+-- Duplicated rows 
+WITH x AS (
+	SELECT count(*), study_areas_id, animals_id, capture_timestamp
+	FROM main.animals_captures JOIN main.animals using (animals_id)
+	GROUP BY animals_id, capture_timestamp, study_areas_id
+	HAVING count(*) > 1
+	ORDER BY study_areas_id, animals_id
+	)
+SELECT study_areas_id, a.*
+from x join main.animals_captures a using (animals_id, capture_timestamp)
+ORDER BY study_areas_id, animals_id, capture_timestamp
+
+
+
+
+
+
+
+
+
