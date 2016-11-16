f3edb215937372d145a3b3b72f8c716b9b469a5a
jedgroev <degroevejohannes@gmail.com>
2016-11-16 11:02:58 +0100

    Update check_mortality_codes.sql

--- data_management/checks/check_mortality_codes.sql
+++ data_management/checks/check_mortality_codes.sql
@@ -1,7 +1,16 @@
--- end_deployment_codes 
+-- End_deployment_codes 
 select * from lu_tables.lu_end_deployment order by 1;
--- mortality_codes 
+-- Statistics deployment 
+SELECT a.end_deployment_code, a.end_deployment_description, count(*) 
+FROM main.gps_sensors_animals JOIN lu_tables.lu_end_deployment a USING (end_deployment_code) 
+GROUP BY 1,2 ORDER BY 3;
+
+-- Mortality_codes 
 select * from lu_tables.lu_mortality order by 1;
+-- Statistics mortality 
+SELECT a.mortality_code, a.mortality_description, count(*) 
+FROM main.gps_sensors_animals JOIN lu_tables.lu_mortality a USING (mortality_code) 
+GROUP BY 1,2 ORDER BY 3;
 
 -- Statistics on end of monitoring and mortality (gps deployments)
 SELECT    b.mortality_code, mortality_description,
@@ -12,7 +21,7 @@ FROM      main.gps_sensors_animals a
   	  LEFT JOIN lu_tables.lu_end_deployment c USING (end_deployment_code)
 GROUP BY  b.mortality_code, mortality_description,
    	  c.end_deployment_code, end_deployment_description
-ORDER BY  end_deployment_description, mortality_description;
+ORDER BY  end_deployment_description, count;
 
 -- Explore suspicious combinations end of deployment/mortality (example gps)
 -- End of deployment is death but mortality code is alive 
@@ -36,19 +45,33 @@ WHERE end_deployment_code IS NULL OR mortality_code IS NULL
 ORDER BY study_areas_id;
 -- 187 animals without deployment and mortality
 
--- number of animals-sensors without end of deployment or mortality code per study area
+-- Number of animals-sensors without end of deployment or mortality code per study area
 SELECT count(*), study_areas_id, end_deployment_code, mortality_code
 FROM main.gps_sensors_animals a JOIN main.animals USING (animals_id)
 WHERE end_deployment_code IS NULL OR mortality_code IS NULL
 GROUP BY study_areas_id, end_deployment_code, mortality_code
 ORDER BY count, study_areas_id
 
--- are there animals that have an end_deployment_code which assumes no data that has data?
+-- Are there animals that have an end_deployment_code which assumes no data that has data?
 SELECT DISTINCT a.* 
 FROM main.gps_sensors_animals a JOIN main.gps_data_animals USING (animals_id,gps_sensors_id) 
 WHERE end_deployment_code in (6,9,11) 
 -- one animal (SA 24 - 2C2T33)
 
+-- MORE SPECIFIC EXPLORATION  
+-- Check animals that are poached. 
+SELECT study_areas_id, a.* 
+FROM main.gps_sensors_animals a JOIN main.animals USING (animals_id) 
+WHERE (a.notes LIKE ('%poach%') OR mortality_code = 9)
+ORDER BY study_areas_id, notes
+
+-- Check all animals-sensors with additional comments for data harmonization 
+SELECT 	study_areas_id, animals_original_id, animals_id, gps_sensors_original_id, gps_sensors_id, 
+	start_time, end_time, end_deployment_code, mortality_code, a.notes 
+FROM 	main.gps_sensors_animals a JOIN main.animals USING (animals_id) 
+	JOIN main.gps_sensors USING (gps_sensors_id) 
+WHERE 	a.notes is not null
+ORDER BY end_deployment_code, study_areas_id, a.notes;
 
 
 ---------------------------------------------------------------------
@@ -65,7 +88,7 @@ FROM      main.vhf_sensors_animals a
   	  LEFT JOIN lu_tables.lu_end_deployment c USING (end_deployment_code)
 GROUP BY  b.mortality_code, mortality_description,
   	  c.end_deployment_code, end_deployment_description
-ORDER BY  end_deployment_description, mortality_description;
+ORDER BY  end_deployment_description, count;
 
 
 -- Explore suspicious combinations end of monitoring/end of deployment (example vhf)
@@ -101,6 +124,5 @@ ORDER BY count, study_areas_id
 SELECT DISTINCT a.* 
 FROM main.vhf_sensors_animals a JOIN main.vhf_data_animals USING (animals_id,vhf_sensors_id) 
 WHERE end_deployment_code in (6,9,11) 
--- one animal (SA 24 - 2C2T33)
 
 
