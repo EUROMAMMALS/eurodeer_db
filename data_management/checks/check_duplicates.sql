f1df12f703cdb7b4515f5436063a80ddb2884c78
jedgroev <degroevejohannes@gmail.com>
2016-11-16 17:39:33 +0100

    Update and rename check_duplicates_sensors_and_or_animals.sql to check_duplicates.sql

--- data_management/checks/check_duplicates.sql
+++ data_management/checks/check_duplicates.sql
@@ -58,21 +58,38 @@ main.animals as a,
 where a.animals_original_id = b.animals_original_id and a.study_areas_id = b.study_areas_id
 ORDER BY animals_original_id;
 
--- 2b Check which of those animals_ids are used in the gps_sensors_animals table 
+-- 2b Check which of those animals_ids are used in the gps_sensors_animals or vhf_sensors_animals table 
+-- GPS
 SELECT DISTINCT animals_id from main.gps_sensors_animals where animals_id in (
 SELECT a.animals_id 
 from 
 main.animals as a, 
 (SELECT animals_original_id, study_areas_id FROM main.animals group by animals_original_id, study_areas_id having count(*) > 1) as b 
 where a.animals_original_id = b.animals_original_id and a.study_areas_id = b.study_areas_id) order by animals_id;
+-- VHF
+SELECT DISTINCT animals_id from main.vhf_sensors_animals where animals_id in (
+SELECT a.animals_id 
+from 
+main.animals as a, 
+(SELECT animals_original_id, study_areas_id FROM main.animals group by animals_original_id, study_areas_id having count(*) > 1) as b 
+where a.animals_original_id = b.animals_original_id and a.study_areas_id = b.study_areas_id) order by animals_id;
 
--- 2c Check which of those animals_ids are used in the gps_sensors_animals table 
+-- 2c Check which of those animals_ids are used in the gps_sensors_animals or vhf_sensors_animals table 
+-- GPS 
 SELECT DISTINCT animals_id from main.gps_data_animals where animals_id in (
 SELECT a.animals_id 
 from 
 main.animals as a, 
 (SELECT animals_original_id, study_areas_id FROM main.animals group by animals_original_id, study_areas_id having count(*) > 1) as b 
 where a.animals_original_id = b.animals_original_id and a.study_areas_id = b.study_areas_id) order by animals_id;
+-- VHF
+SELECT DISTINCT animals_id from main.vhf_data_animals where animals_id in (
+SELECT a.animals_id 
+from 
+main.animals as a, 
+(SELECT animals_original_id, study_areas_id FROM main.animals group by animals_original_id, study_areas_id having count(*) > 1) as b 
+where a.animals_original_id = b.animals_original_id and a.study_areas_id = b.study_areas_id) order by animals_id;
+
 
 -- 3 DUPLICATE ANIMALS-SENSORS 
 -- 3a List of all animals-sensors with duplicate animals-sensors id (possible) -> check start and end times to confirm potential duplicates
@@ -88,7 +105,6 @@ from
 main.vhf_sensors_animals as a,
 (SELECT animals_id, vhf_sensors_id FROM main.vhf_sensors_animals group by vhf_sensors_id, animals_id having count(*) > 1) as b
 where a.animals_id = b.animals_id and a.vhf_sensors_id = b.vhf_sensors_id;
-
 
 
 -- 4 DUPLICATED CAPTURES 
@@ -111,10 +127,17 @@ from x join main.animals_captures a using (animals_id, capture_timestamp)
 ORDER BY study_areas_id, animals_id, capture_timestamp
 
 
-
-
-
-
-
-
-
+-- CONTACT DATA 
+-- Are there duplicate contacts? 
+SELECT count(*), animals_id, contact_timestamp, study_areas_id
+FROM main.animals_contacts join main.animals using (animals_id)
+GROUP BY animals_id, contact_timestamp, study_areas_id
+HAVING count(*) > 1
+-- Duplicated rows 
+WITH x AS (
+	SELECT count(*), animals_id, contact_timestamp, study_areas_id
+	FROM main.animals_contacts join main.animals using (animals_id)
+	GROUP BY animals_id, contact_timestamp, study_areas_id
+	HAVING count(*) > 1
+	)
+SELECT a.* FROM x join main.animals_contacts a using (animals_id, contact_timestamp)
