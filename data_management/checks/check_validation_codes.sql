20485b4a473e165cce7c9d49628cd65c27b57c6a
jedgroev <degroevejohannes@gmail.com>
2016-11-16 18:03:05 +0100

    Update check_validation_codes.sql

--- data_management/checks/check_validation_codes.sql
+++ data_management/checks/check_validation_codes.sql
@@ -1,32 +1,16 @@
-﻿-- fixes outside deployment but not 14
+-- fixes outside deployment but not 14
 select * from main.gps_data_animals where
 gps_data_animals_id not in
-(SELECT
-gps_data_animals.gps_data_animals_id
-FROM
-main.gps_data_animals,
-main.gps_sensors_animals
-WHERE
-gps_data_animals.animals_id = gps_sensors_animals.animals_id AND
-gps_data_animals.gps_sensors_id = gps_sensors_animals.gps_sensors_id and
-(gps_data_animals.acquisition_time between gps_sensors_animals.start_time and gps_sensors_animals.end_time)) and
-
-gps_data_animals.gps_validity_code != 14
+(SELECT gps_data_animals_id
+FROM main.gps_data_animals a JOIN main.gps_sensors_animals b USING (animals_id, gps_sensors_id)
+WHERE acquisition_time between start_time and end_time) and gps_validity_code != 14
 order by gps_validity_code;
 
 -- fixes inside deployment but 14
 select * from main.gps_data_animals where
 gps_data_animals_id in
-(SELECT
-gps_data_animals.gps_data_animals_id
-FROM
-main.gps_data_animals,
-main.gps_sensors_animals
-WHERE
-gps_data_animals.animals_id = gps_sensors_animals.animals_id AND
-gps_data_animals.gps_sensors_id = gps_sensors_animals.gps_sensors_id and
-(gps_data_animals.acquisition_time between gps_sensors_animals.start_time and gps_sensors_animals.end_time)) and
-
-gps_data_animals.gps_validity_code = 14
+(SELECT gps_data_animals.gps_data_animals_id
+FROM main.gps_data_animals a JOIN main.gps_sensors_animals USING (animals_id, gps_sensors_id)
+WHERE acquisition_time between start_time and end_time) and gps_validity_code = 14
 order by gps_validity_code;
 
