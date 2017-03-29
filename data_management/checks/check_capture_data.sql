2b21c4565173ce5c355299f81c996b047d9bd858
jedgroev <degroevejohannes@gmail.com>
2017-03-29 17:18:47 +0200

    Update check_capture_data.sql

--- data_management/checks/check_capture_data.sql
+++ data_management/checks/check_capture_data.sql
@@ -1,2 +1,24 @@
 -- is capture timestamp larger than release timestamp
-SELECT study_areas_id, animals_original_id, a.* FROM main.animals_captures a JOIN main.animals using (animals_id) WHERE capture_timestamp > release_timestamp 
+SELECT study_areas_id, animals_original_id, a.* 
+FROM main.animals_captures a JOIN main.animals using (animals_id) 
+WHERE capture_timestamp > release_timestamp 
+
+-- animals for which handling_start is not between capture_timestamp and release_timestamp 
+SELECT study_areas_id, animals_original_id, a.* 
+FROM main.animals_captures a JOIN main.animals using (animals_id) 
+WHERE handling_start not between capture_timestamp and release_timestamp 
+
+-- animals for which handling_start is not between capture_timestamp and release_timestamp 
+SELECT study_areas_id, animals_original_id, a.* 
+FROM main.animals_captures a JOIN main.animals using (animals_id) 
+WHERE handling_end not between capture_timestamp and release_timestamp 
+
+-- animals for which heart_rate_start_timestamp is not between capture_timestamp and release_timestamp
+SELECT study_areas_id, animals_original_id, a.* 
+FROM main.animals_captures a JOIN main.animals using (animals_id) 
+WHERE heart_rate_start_timestamp not between capture_timestamp and release_timestamp
+
+-- animals for which heart_rate_end_timestamp is not between capture_timestamp and release_timestamp
+SELECT study_areas_id, animals_original_id, a.* 
+FROM main.animals_captures a JOIN main.animals using (animals_id) 
+WHERE heart_rate_end_timestamp not between capture_timestamp and release_timestamp
