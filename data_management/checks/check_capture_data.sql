7117b5b2ead92c883084979353b32bc284e7e3c8
jedgroev <degroevejohannes@gmail.com>
2017-03-31 14:31:32 +0200

    Update check_capture_data.sql

--- data_management/checks/check_capture_data.sql
+++ data_management/checks/check_capture_data.sql
@@ -22,3 +22,9 @@ WHERE heart_rate_start_timestamp not between capture_timestamp and release_times
 SELECT study_areas_id, animals_original_id, a.* 
 FROM main.animals_captures a JOIN main.animals using (animals_id) 
 WHERE heart_rate_end_timestamp not between capture_timestamp and release_timestamp
+
+-- animals with capture result code 1, 4, 5 but died during or right after capture 
+SELECT * FROM main.animals_captures WHERE capture_result_code in (1,4,5) and death = TRUE; 
+
+-- animals with capture result code 1, 4, 5 but died during or right after capture 
+SELECT * FROM main.animals_captures WHERE capture_result_code in (2,3) and death = FALSE; 
