9d3e051d903eedb254efc72d6bb76e8460c25ea4
jedgroev <degroevejohannes@gmail.com>
2017-05-22 14:12:41 +0200

    Update check_geometry.sql

--- data_management/checks/check_geometry.sql
+++ data_management/checks/check_geometry.sql
@@ -1,8 +1,6 @@
--- Wrong tags 
-SELECT animals_id, latitude, longitude, geom FROM main.gps_data_animals 
-WHERE longitude is null and geom is not null;
-SELECT animals_id, latitude, longitude, geom FROM main.gps_data_animals 
-WHERE latitude is null and geom is not null;
+-- longitude/latitude is null but geometry is not
+SELECT animals_id, latitude, longitude, geom FROM main.gps_data_animals WHERE longitude is null and geom is not null;
+SELECT animals_id, latitude, longitude, geom FROM main.gps_data_animals WHERE latitude is null and geom is not null;
 
 -- geom and longitude/latitude do not correspond
 SELECT gps_data_animals_id, study_areas_id, animals_id, latitude, longitude, geom, st_y(geom),st_x(geom), gps_validity_code 
@@ -19,8 +17,14 @@ SELECT * from main.gps_data_animals where gps_validity_code in (11) and utm_x is
 -- UPDATE main.gps_data_animals SET utm_x = NULL, utm_y = NULL WHERE gps_validity_code in (11) and utm_x is not null; 
 
 -- check if utm_x is consistent with the geom
-SELECT animals_id, latitude, longitude, geom, gps_validity_code FROM main.gps_data_animals 
-WHERE utm_x != st_x(st_transform(geom,utm_srid)) and gps_validity_code = 1;
-SELECT animals_id, latitude, longitude, geom, gps_validity_code FROM main.gps_data_animals 
-WHERE utm_y != st_y(st_transform(geom,utm_srid)) and gps_validity_code = 1;
+SELECT animals_id, latitude, longitude, geom, gps_validity_code, utm_x, utm_y,
+st_x(st_transform(geom,utm_srid))::integer,st_y(st_transform(geom,utm_srid))::integer FROM main.gps_data_animals 
+WHERE utm_x != st_x(st_transform(geom,utm_srid))::integer and gps_validity_code = 1; 
+-- check if utm_y is consistent with the geom
+SELECT animals_id, latitude, longitude, geom, gps_validity_code, utm_x, utm_y,
+st_x(st_transform(geom,utm_srid))::integer,st_y(st_transform(geom,utm_srid))::integer FROM main.gps_data_animals 
+WHERE utm_y != st_y(st_transform(geom,utm_srid))::integer and gps_validity_code = 1; 
+
+
+
 -- UPDATE main.gps_data_animals SET latitude = st_y(geom), longitude = st_x(geom) WHERE latitude != st_y(geom) and longitude != st_x(geom)
