63cbcda66139bdbaafc279769af7e8b5c6b08940
feurbano <ferdi.urbano@gmail.com>
2018-02-06 11:41:44 +0100

    Update generate_dictionary.sql

--- documentation/generate_database_dictionary_sql/generate_dictionary.sql
+++ documentation/generate_database_dictionary_sql/generate_dictionary.sql
@@ -1,4 +1,8 @@
-﻿select report as "# DATABASE REPORT: eurodeer_db SCHEMAS and TABLES" from 
+SELECT report as "# DATABASE REPORT: eurodeer_db SCHEMAS and TABLES" -- The name of the column is used as main title
+FROM 
+-- I generate the list of schemas, tables and fields with their comments (excluding some elements, e.g. tables in working schema)
+-- I format the text in MD
+-- I union the three elements and I order by schema, table and columns position
 (SELECT table_name tablex, table_schema schemax, relkind, 0 as ordex, 
 '### ' || case when relkind = 'r' then 'TABLE: ' when relkind = 'v' then 'VIEW: ' when relkind = 'e' then 'EXTERNAL TABLE: ' end || replace(table_schema,'_','\_') || '.' || replace(table_name,'_','\_')  || E'  \n' || 
 replace(coalesce(obj_description(oid),''),'_','\_') || E'  \n\n' || case when relkind = 'r' then E'#### COLUMNS \n\n' else '' end  as report
@@ -18,6 +22,8 @@ FROM information_schema.columns, information_schema.tables, pg_class
 WHERE tables.table_schema in (select nspname from pg_catalog.pg_namespace WHERE nspname !~ '^pg_' AND nspname not in ( 'information_schema','public','temp', 'activity_data_raw') and nspname not like 'ws_%')
  and  columns.table_schema = tables.table_schema and tables.table_name= relname and tables.table_name= columns.table_name and pg_class.relkind in ('r')
 ) q
+-- I order by schema (with a predefined order) then by table name (first table, then view and external tables) and by column position
+-- It could simply be schemax, relkind, tablex, ordex, but I want to decide the order of the schemas
 order by 
 schemax = 'main' DESC,
 schemax = 'lu_tables' DESC,
