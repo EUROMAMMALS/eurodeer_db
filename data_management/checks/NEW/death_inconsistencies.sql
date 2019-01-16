e94440352e31df93838238ae15ca1c2667f2aedb
jedgroev <degroevejohannes@gmail.com>
2019-01-16 14:22:04 +0100

    Create death_inconsistencies.sql

---
+++ data_management/checks/NEW/death_inconsistencies.sql
@@ -0,0 +1,24 @@
+-- capture after death for animals with end of deployment reason 'death'
+SELECT animals_id, start_time, end_time, capture_timestamp
+FROM main.animals_captures JOIN main.gps_sensors_animals USING (animals_id) 
+WHERE end_deployment_code = 4 and capture_timestamp > end_time;
+
+-- animals death_date before capture_timestamp
+SELECT animals_id, death_date, death_time, capture_timestamp
+FROM main.animals JOIN main.animals_captures USING (animals_id) 
+WHERE  capture_timestamp > (death_date || ' ' ||death_time )::timestamp with time zone;
+
+-- end of deployment and death date inconsistent
+SELECT animals_id, death_date, end_time FROM main.animals JOIN main.gps_sensors_animals USING (animals_id)
+WHERE end_deployment_code = 4 and end_time != (death_date || ' ' || death_time)::timestamp with time zone;
+
+-- VHF
+-- capture after death for animals with end of deployment reason 'death'
+SELECT study_areas_id, study_name, animals_id, start_time, end_time, capture_timestamp, end_deployment_code
+FROM main.animals_captures JOIN main.vhf_sensors_animals USING (animals_id) 
+join main.animals using (animals_id) join main.study_Areas using (study_areas_id)
+WHERE end_deployment_code = 4 and capture_timestamp > end_time;
+
+-- end of deployment and death date inconsistent
+SELECT animals_id, death_date, end_time FROM main.animals JOIN main.vhf_sensors_animals USING (animals_id)
+WHERE end_deployment_code = 4 and end_time != (death_date || ' ' || death_time)::timestamp with time zone;
